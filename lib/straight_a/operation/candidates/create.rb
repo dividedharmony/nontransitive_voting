58ecaff983bc6d0b2c3fb853edeef15d1f0e7591
David Harmon <dharmon202@gmail.com>
2017-03-01 09:53:28 -0500

    Add Operation::Candidates::Create

---
+++ lib/straight_a/operation/candidates/create.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module StraightA
+  module Operation
+    module Candidates
+      class Create
+        attr_reader :anime, :award
+
+        # Use +find_by+ instead of +find+ so as not to
+        # throw an ActiveRecord::RecordNotFound
+        #
+        def initialize(params)
+          @anime = Anime.find_by(id: params[:candidate][:source_id])
+          @award = Award.find_by(id: params[:candidate][:award_id])
+        end
+
+        def operable?
+          anime.present? && award.present?
+        end
+
+        def operate!
+          Candidate.create!(source: anime, award: award)
+        end
+      end
+    end
+  end
+end
