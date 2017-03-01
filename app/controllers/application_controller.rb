8001792923853ebe780d9e49233669f7c5c8255e
David Harmon <dharmon202@gmail.com>
2017-03-01 09:52:28 -0500

    Add #run and #form methods to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery with: :exception
+
+  def run(operation_class, success: nil, failure: nil)
+    @operation = operation_class.new(params)
+    if @operation.operable?
+      @operation.operate!
+      method(success) if success
+    elsif failure
+      method(failure).call
+    end
+  end
+
+  def form(formula_class, failure: nil)
+    @formula = formula_class.new(params)
+    if @formula.formable?
+      @formula.formulate!
+    elsif failure
+      method(failure).call
+    end
+  end
 end
