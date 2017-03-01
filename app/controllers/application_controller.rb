1f75be112274d3460d0d600f93f628a5430b5ec3
David Harmon <dharmon202@gmail.com>
2017-03-01 11:10:51 -0500

    Require straight_a namespace in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require 'straight_a'
+
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery with: :exception
