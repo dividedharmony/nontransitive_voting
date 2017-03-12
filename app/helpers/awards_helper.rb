cbf0c3d6e79e31ef71722e8ef23c83a154fe635e
David Harmon <dharmon202@gmail.com>
2017-03-12 18:09:07 -0400

    Cache awards on award index page

--- app/helpers/awards_helper.rb
+++ app/helpers/awards_helper.rb
@@ -4,4 +4,8 @@ module AwardsHelper
   def begin_voting_path(award)
     show_ballot_path(id: Ballot.first_for(award).id)
   end
+
+  def award_cache_expire_time(award)
+    award.award_season.voting_ends_at - Time.zone.now
+  end
 end
