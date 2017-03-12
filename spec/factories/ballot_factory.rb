2ad6ce35cc5d8510f5fcc57c5f321208a2c427cd
David Harmon <dharmon202@gmail.com>
2017-03-12 17:21:00 -0400

    Refactor ballot factory to allow user to pass in award param

--- spec/factories/ballot_factory.rb
+++ spec/factories/ballot_factory.rb
@@ -2,7 +2,17 @@
 
 FactoryGirl.define do
   factory :ballot do
-    association :candidate_a, factory: [:candidate, :ballot_friendly]
-    association :candidate_b, factory: [:candidate, :ballot_friendly]
+    candidate_a nil
+    candidate_b nil
+
+    transient do
+      award { Award.first || create(:award) }
+    end
+
+    after(:build) do |ballot, evaluator|
+      ballot.candidate_a ||= create(:candidate, award: evaluator.award)
+      ballot.candidate_b ||= create(:candidate, award: evaluator.award)
+      ballot.save!
+    end
   end
 end
